bf999320fb3ee27a6c604eeceab4706cb5db0943
Juan Leal <juan.leal.higuera@gmail.com>
2024-03-25 23:02:11 -0500

    [+] se realiza listado de items de side menu asi como tambien logica para renderizar cada item desde el ts

--- src/app/modules/administration/administration.component.ts
+++ src/app/modules/administration/administration.component.ts
@@ -8,8 +8,34 @@ import { Component } from '@angular/core';
 export class AdministrationComponent {
 
   isExpanded = true
+  isExpandedSubmenu = false
+
+  Menus = [
+    {title: "Regresar al POS"},
+    {title: "Reportes", icon: 'fa-regular fa-file-lines'},
+    {title: "Empleados", spacing: true, icon: 'fa-regular fa-image'},
+    {
+      title: "Productos",
+      icon: 'fa-regular fa-folder-open',
+      submenu: true,
+      submenuItems: [
+        {title: "Submenu 1"},
+        {title: "Submenu 2"},
+        {title: "Submenu 3"}
+      ],
+    },
+    {title: "Analytics", icon: 'fa-solid fa-chart-simple'},
+    {title: "Inventario", icon: 'fa-solid fa-inbox'},
+    {title: "Información fiscal", spacing: true, icon: 'fa-regular fa-user'},
+    {title: "Configuraciones", icon: 'fa-solid fa-gear'},
+    {title: "Cerrar sesión", icon: 'fa-solid fa-right-from-bracket'}
+  ]
 
   toggleMenu() {
     this.isExpanded = !this.isExpanded
+  }
+
+  toggleSubmenu () {
+    this.isExpandedSubmenu = !this.isExpandedSubmenu
   }
 }
